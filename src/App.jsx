a6cce7ce8690eff3cb5169fd49ceaaf10817df84
Goutham Arelli <arelligoutham@gmail.com>
2025-01-13 13:20:14 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button, DatePicker, Space, version } from "antd";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h3>Stay tuned for NutriX-Connect</h3>
+    </>
+  );
+}
+
+export default App;
