4e739e4c390cfd7389b0311b54b5582dc99e741c
Goutham Arelli <arelligoutham@gmail.com>
2025-01-30 20:03:45 +0530

    Added Razorpay

---
+++ src/middleware/orderMiddleware.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { razorpayCheckout } from "./razorpayMiddleware";
+
+const ORDER_DETAILS_API = `${import.meta.env.VITE_API_URL}/api/order`;
+
+export const createOrder =
+  (planId, dietitianId, user, status = "pending") =>
+  async (dispatch) => {
+    try {
+      const response = await axios.post(`${ORDER_DETAILS_API}/${planId}`, {
+        dietitianId,
+        userId: user.email,
+        status,
+      });
+
+      dispatch(razorpayCheckout(response.data), user);
+    } catch (error) {
+      alert("Error: " + error.message);
+      return;
+    }
+  };
